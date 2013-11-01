90d04622b349ef22988998f7391322fa368060a4
Diego Daguerre <diegodc.uy@gmail.com>
2013-11-01 20:50:51 -0200

    agregado modulo getValue

---
+++ twisterzoom/get_value.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""
+Esta función se encarga de convertir información que envía la estación
+metereogica en una linea a un diccionario.
+
+De esto:
+WIND 	   6.5	  277
+
+A esto:
+['WIND',6.5,277]
+
+"""
+
+def getValue(raw_line):
+	
+	print "RAW LINE: " + raw_line
+	
+	return values
