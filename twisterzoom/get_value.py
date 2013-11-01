b4073c7ed7bc276e0803f83e72f189345956f1d9
pablok <pablodav@gmail.com>
2013-11-01 21:29:19 -0200

    funcional

--- twisterzoom/get_value.py
+++ twisterzoom/get_value.py
@@ -12,9 +12,21 @@ A esto:
 ['WIND',6.5,277]
 
 """
+import re
+
 
 def getValue(raw_line):
-	
-	print "RAW LINE: " + raw_line
-	
-	#return values
+    print "RAW LINE: " + raw_line
+    #return values
+
+if __name__ == "__main__":
+    a = "algo"
+    getValue(a)
+  
+  
+"""
+example: 
+from get_value import getValue
+getValue("WIND 6.34 234")
+RAW LINE: WIND 6.34 234
+"""
