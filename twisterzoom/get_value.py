a820a08e48c242bd6d559619034396b086587ad9
pablok <pablodav@gmail.com>
2013-11-05 22:33:50 -0200

    Agregados datos a get_value

--- twisterzoom/get_value.py
+++ twisterzoom/get_value.py
@@ -11,16 +11,40 @@ WIND 	   6.5	  277
 A esto:
 ['WIND',6.5,277]
 
+Pablo: Estuve probando con los datos tal cual los copiamos en una consola python y encontré que hay que usar tabuladores debido a esto: 
+import re
+raw_line = "WIND       6.5    277"
+>>> raw_line
+'WIND \t   6.5\t  277'
+
+Entonces probando: 
+>>> print re.split(r'\t', raw_line)
+['WIND ', '   6.5', '  277']
+
+Quitando los espacios:
+split_line = re.split(r'\t', raw_line)
+>>> WIND = [x.strip(' ') for x in split_line]
+>>> WIND
+Tuve problemas con el print
+
 """
 import re
 
 
 def getValue(raw_line):
-    print "RAW LINE: " + raw_line
-    #return values
+    print "Datos raw: " + raw_line
+    split_line = re.split(r'\t', raw_line)
+    print "Datos split: "
+    print split_line
+    WIND = [x.strip(' ') for x in split_line]
+    print "Datos WIND Procesados:"
+    print WIND
+    
+
+
 
 if __name__ == "__main__":
-    a = "algo"
+    a = "WIND 	   6.5	  27"
     getValue(a)
   
   
