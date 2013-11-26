001a8c1a00111b4ce4a86a5ad1166e61ae4618ad
pablok <pablodav@gmail.com>
2013-11-26 17:09:23 -0200

    Datos extraídos.

--- twisterzoom/test_socket.py
+++ twisterzoom/test_socket.py
@@ -9,12 +9,35 @@ num = 0
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-entradas = [s]
+entrada = [s]
 
 while num < 50:
-    entrada, salida, errores = select.select(entrada, [], [])
-	data = s.recv(1024)
-	print 'Received', repr(data)
-	num = num +1
+    entrada, salida, errores = select.select(entrada, [], []) 
+    data = s.recv(4096)
+    print 'Received', repr(data)
+    num = num +1
 
 s.close()
+
+"""
+Received '\x01WIND\x02\t   4.6\t  130\t\x03\r\n'
+Received '\x01WIND\x02\t   4.1\t  122\t\x03\r\n'
+Received '\x01WIND\x02\t   5.0\t  131\t\x03\r\n'
+Received '\x01WIND\x02\t   6.0\t  125\t\x03\r\n'
+Received '\x01WIND\x02\t   4.6\t  125\t\x03\r\n'
+Received '\x01WIND\x02\t   4.6\t  142\t\x03\r\n'
+Received '\x01WIND\x02\t   4.4\t  133\t\x03\r\n'
+Received '\x01WIND\x02\t   5.5\t  133\t\x03\r\n'
+Received '\x01WIND\x02\t   4.5\t  130\t\x03\r\n'
+Received '\x01WIND\x02\t   4.1\t  120\t\x03\r\n'
+Received '\x01PTU\x02\t23.5  \t23.9  \t22.8  \t70  \t75  \t69  \t17.9  \t18.4  \t17.4  \t1'
+Received '006.2\t1007.1\t1006.1\t225  \t495  \t142  \t0.0   \t0.0   \t0.0   \t0.0  '
+Received ' \t0.0   \t0.0   \t0.0   \t\x03\r\n'
+Received '\x01WIND\x02\t   4.1\t  120\t\x03\r\n'
+Received '\x01WIND\x02\t   5.3\t  148\t\x03\r\n'
+Received '\x01WIND\x02\t   5.3\t  136\t\x03\r\n'
+Received '\x01WIND\x02\t   5.0\t  126\t\x03\r\n'
+Received '\x01WIND\x02\t   5.7\t  129\t\x03\r\n'
+Received '\x01WIND\x02\t   5.0\t  133\t\x03\r\n'
+
+"""
